f3911a38878069a2728bb570ff1fd970f1bb6c15
Sean Oldham <seanosaur@me.com>
2015-02-02 20:56:21 -0600

    take a gem name, return info, gem_uri

--- rubygems_query.rb
+++ rubygems_query.rb
@@ -1,3 +1,16 @@
-puts "Search for a gem: "
-gem_search = gets.chomp
-puts "http://rubygems.com/search?query=#{gem_search}"
+require 'rubygems'
+require 'gems'
+
+def search(gem_name)
+  gem_info = Gems.info gem_name
+  if gem_name == gem_info["name"]
+    puts gem_info["info"]
+    puts gem_info["gem_uri"]
+  else
+    puts "Gem does not exist. Try again."
+  end
+end
+
+puts "Enter a gem name: "
+gem_name = gets.chomp
+search(gem_name)
