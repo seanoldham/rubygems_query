f3679fd3dfe9476d07f6a126bb557d679a63bf94
Sean Oldham <seanosaur@me.com>
2015-02-02 21:08:32 -0600

    output more URLs with basic description

--- rubygems_query.rb
+++ rubygems_query.rb
@@ -4,8 +4,10 @@ require 'gems'
 def search(gem_name)
   gem_info = Gems.info gem_name
   if gem_name == gem_info["name"]
-    puts gem_info["info"]
-    puts gem_info["gem_uri"]
+    puts gem_info['info']
+    puts "RubyGems: #{gem_info['project_uri']}"
+    puts "Homepage: #{gem_info['homepage_uri']}"
+    puts "Documentation: #{gem_info['documentation_uri']}"
   else
     puts "Gem does not exist. Try again."
   end
