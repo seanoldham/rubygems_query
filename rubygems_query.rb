a6d1f7805b2053c7d31fa1b565f425b9d44d1341
Sean Oldham <seanosaur@me.com>
2015-02-09 15:45:07 -0600

    initial rails commit

---
+++ rubygems_query.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'gems'
+
+def search(gem_name)
+  gem_info = Gems.info gem_name
+  if gem_name == gem_info["name"]
+    puts gem_info['info']
+    puts "RubyGems: #{gem_info['project_uri']}"
+    puts "Homepage: #{gem_info['homepage_uri']}"
+    puts "Documentation: #{gem_info['documentation_uri']}"
+  else
+    puts "Gem does not exist. Try again."
+  end
+end
+
+puts "Enter a gem name: "
+gem_name = gets.chomp
+search(gem_name)
