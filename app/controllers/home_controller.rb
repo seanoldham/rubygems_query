e7623535d19ac9726c2804f9f4db1ea9be013846
Sean Oldham <seanosaur@me.com>
2015-02-09 16:42:20 -0600

    create index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,18 @@
 class HomeController < ApplicationController
+
+  def index
+    
+  end
+
   def search(gem_name)
+    try_again = "Gem does not exist. Try again."
     gem_info = Gems.info params[:text]
     if gem_name == gem_info["name"]
       @project_uri = gem_info['project_uri']
       @homepage = gem_info['homepage_uri']
       @docs = gem_info['documentation_uri']
     else
-      return "Gem does not exist. Try again."
+      return try_again
     end
   end
 end
