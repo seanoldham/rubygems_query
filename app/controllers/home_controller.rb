655a667eac1a0d3ce1668770db156fbe701b5d4c
Sean Oldham <seanosaur@me.com>
2015-02-09 16:12:21 -0600

    add gem_name variable to method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def search
+    gem_name = params[:text]
     gem_info = Gems.info params[:text]
     if gem_name == gem_info["name"]
       return gem_info["info"] + "\n" + "RubyGems: #{gem_info['project_uri']}" + "\n" + "Homepage: #{gem_info['homepage_uri']}" + "\n" + "Documentation: #{gem_info['documentation_uri']}"
