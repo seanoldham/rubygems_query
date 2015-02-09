2f1aa34f969e762ad5424949393796fff1a06098
Sean Oldham <seanosaur@me.com>
2015-02-09 16:30:11 -0600

    refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
-  def search
-    gem_name = params[:text]
+  def search(gem_name)
     gem_info = Gems.info params[:text]
     if gem_name == gem_info["name"]
-      return gem_info["info"] + "\n" + "RubyGems: #{gem_info['project_uri']}" + "\n" + "Homepage: #{gem_info['homepage_uri']}" + "\n" + "Documentation: #{gem_info['documentation_uri']}"
+      @project_uri = gem_info['project_uri']
+      @homepage = gem_info['homepage_uri']
+      @docs = gem_info['documentation_uri']
     else
       return "Gem does not exist. Try again."
     end
