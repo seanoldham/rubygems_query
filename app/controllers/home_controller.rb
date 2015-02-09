92018cab650c27a4fedc7b8e2dfca223e9ef0c23
Sean Oldham <seanosaur@me.com>
2015-02-09 15:56:50 -0600

    push to heroku

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def search
+    gem_info = Gems.info params[:text]
+    if gem_name == gem_info["name"]
+      return gem_info["info"] + "\n" + "RubyGems: #{gem_info['project_uri']}" + "\n" + "Homepage: #{gem_info['homepage_uri']}" + "\n" + "Documentation: #{gem_info['documentation_uri']}"
+    else
+      return "Gem does not exist. Try again."
+    end
+  end
+end
