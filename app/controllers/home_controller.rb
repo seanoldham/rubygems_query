18bf7214d114ec43f8f130271c29a93dd53a73e8
slacker87 <racethesunlive@yahoo.com>
2015-02-10 16:31:50 +0000

    turn the gem link into a proper html link in slack

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,19 +6,21 @@ class HomeController < ApplicationController
   end
 
   def search
-    @gem_name = params[:text]
-    gem_info = Gems.info @gem_name
-    if @gem_name.present?
-      @text = gem_info["project_uri"]
+    gem_name = params[:text]
+    gem_info = Gems.info gem_name
+    if gem_name.present?
+      text = gem_info["project_uri"]
       #@project_uri = gem_info["project_uri"]
       #@homepage = gem_info["homepage_uri"]
       #@docs = gem_info["documentation_uri"]
-      if !@text.present?
-        @text = "gem not found"
+      if !text.present?
+        return render json: "gem not found"
+      else
+        project_uri = "<#{gem_info["project_uri"]}>"
+        return render json: project_uri
       end
-      return render json: @text
     else
-      return render json: "no gems found"
+      return render json: "Please enter a gem name"
     end
   end
 end
