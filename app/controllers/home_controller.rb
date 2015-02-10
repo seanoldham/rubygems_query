ad902f8456a1175d0ef160a9489e30fedf7f9545
Sean Oldham <seanosaur@me.com>
2015-02-10 10:41:44 -0600

    add text param to search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,20 @@
 class HomeController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  require 'gems'
 
   def index
     @gem_name = params[:text]
   end
 
   def search
+    @gem_name = params[:text]
     gem_info = Gems.info @gem_name
     if @gem_name == gem_info["name"]
       # @project_uri = gem_info["project_uri"]
       # @homepage = gem_info["homepage_uri"]
       # @docs = gem_info["documentation_uri"]
-      render json: gem_info["name"]
+      all_gem_info = "#{gem_info['name']} " + "#{gem_info['project_uri']} " + "#{gem_info['homepage_uri']} " + "#{gem_info['documentation_uri']}"
+      render json: all_gem_info
     else
       # @project_uri = "None found"
       # @homepage = ""
